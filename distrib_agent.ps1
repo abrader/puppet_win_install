e4d62a93f04649fe5d132a086c63a03820f82915
Andrew Brader <abrader@gmail.com>
2016-06-24 15:27:19 -0400

    add new test

--- distrib_agent.ps1
+++ distrib_agent.ps1
@@ -4,12 +4,12 @@
 Param(
   [string]$server      = "<%= @server_setting %>",
   [string]$certname    = $null,
-  [string]$install_dest    = "$env:temp\puppet-agent-x64.msi",
+  [string]$install_dest    = "install.ps1.eps",
   [string]$install_source  = 'https://raw.githubusercontent.com/trevharmon/puppet-pe_install_ps1/METHOD-653/templates/install.ps1.erb',
   [string]$install_log = "$env:temp\puppet-install.log"
 )
 # Uncomment the following line to enable debugging messages
-$DebugPreference = 'Continue'
+# $DebugPreference = 'Continue'
 
 function DownloadInstallPS1 {
   Write-Verbose "Downloading the Puppet Agent for Puppet Enterprise <%= @pe_build %> on $env:COMPUTERNAME..."
@@ -19,9 +19,8 @@ function DownloadInstallPS1 {
     $webclient.DownloadFile($install_src,$install_dest)
   }
   catch [Net.WebException] {
-    Write-Warning "Failed to download the Puppet Agent installer: ${msi_source}"
+    Write-Warning "Failed to download the install template: ${install_src}"
     Write-Warning "$_"
-    Write-Warning "Does the Puppet Master have the pe_repo::platform::windows_<arch> class applied to it?"
     break
   }
 }
