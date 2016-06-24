2c6a25b61985c33cbf203c0181e36d1df54f080b
Andrew Brader <abrader@gmail.com>
2016-06-24 15:30:39 -0400

    Fixes

--- distrib_agent.ps1
+++ distrib_agent.ps1
@@ -5,14 +5,14 @@ Param(
   [string]$server      = "<%= @server_setting %>",
   [string]$certname    = $null,
   [string]$install_dest    = "install.ps1.eps",
-  [string]$install_source  = 'https://raw.githubusercontent.com/trevharmon/puppet-pe_install_ps1/METHOD-653/templates/install.ps1.erb',
+  [string]$install_src  = 'https://raw.githubusercontent.com/trevharmon/puppet-pe_install_ps1/METHOD-653/templates/install.ps1.erb',
   [string]$install_log = "$env:temp\puppet-install.log"
 )
 # Uncomment the following line to enable debugging messages
 # $DebugPreference = 'Continue'
 
 function DownloadInstallPS1 {
-  Write-Verbose "Downloading the Puppet Agent for Puppet Enterprise <%= @pe_build %> on $env:COMPUTERNAME..."
+  Write-Verbose "Downloading the install template on $env:COMPUTERNAME..."
   [System.Net.ServicePointManager]::ServerCertificateValidationCallback={$true}
   $webclient = New-Object system.net.webclient
   try {
